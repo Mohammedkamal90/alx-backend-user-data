1c359c8a4e27d109eee13d95b95e2565be4f3285
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-18 04:51:34 +0000

    upd

--- 0x01-Basic_authentication/main_6.py
+++ 0x01-Basic_authentication/main_6.py
@@ -16,11 +16,3 @@ user.save()
 
 basic_clear = "{}:{}".format(user_email, user_clear_pwd)
 print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
-
-bob@dylan:~$
-bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 ./main_6.py 
-New user: 9375973a-68c7-46aa-b135-29f79e837495 / bob@hbtn.io
-Basic Base64: Ym9iQGhidG4uaW86SDBsYmVydG9uU2Nob29sOTgh
-bob@dylan:~$
-bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 AUTH_TYPE=basic_auth python3 -m api.v1.app
- * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
