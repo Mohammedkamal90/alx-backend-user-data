8501cee2ccaf55759de807aaad94753774837bde
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-19 22:27:20 +0000

    upd

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,22 +5,74 @@ Module for Session Authentication.
 from api.v1.auth.auth import Auth
 import uuid
 
-class SessionAuth(Auth):
-    """Session Authentication"""
-    user_id_by_session_id = {}
+import uuid
+
+class SessionAuth:
+    """
+    Session authentication mechanism
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Create a Session ID for a user_id."""
-        if not isinstance(user_id, str):
+        """
+        Create a Session ID for a user
+        """
+        if user_id is None or not isinstance(user_id, str):
             return None
-        
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Return a User ID based on a Session ID."""
+        """
+        Return the User ID based on a Session ID
+        """
         if session_id is None or not isinstance(session_id, str):
             return None
-        
         return self.user_id_by_session_id.get(session_id)
+
+# Test code
+sa = SessionAuth()
+
+print("{}: {}".format(type(sa.user_id_by_session_id), sa.user_id_by_session_id))
+
+user_id = None
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = 89
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "abcde"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "fghij"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "abcde"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+tmp_session_id = None
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = 89
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = "doesntexist"
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = session
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
