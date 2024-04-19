0f03309fd4c9667a1e7826d11af2220826a77609
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-19 22:15:11 +0000

    upd

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,40 +1,27 @@
 #!/usr/bin/env python3
 """
+Module for Session Authentication.
 module Session Authentication
 """
-from flask import jsonify, request
-from api.v1.views import app_views
-from models.user import User
-from api.v1.app import auth
+from api.v1.auth.auth import Auth
+import uuid
 
 class SessionAuth(Auth):
-    """
-    SessionAuth class
-    """
+    """Session Authentication"""
+    pass
+    user_id_by_session_id = {}
 
-    def __init__(self):
-        """
-        Initialize SessionAuth instance
-        """
-        super().__init__()
-
-    def current_user(self, request=None):
-        """
-        Return the User instance based on the cookie value
-        """
-        if request is None:
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id."""
+        if not isinstance(user_id, str):
             return None
 
-        session_id = self.session_cookie(request)
-
-        if session_id is None:
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return a User ID based on a Session ID."""
+        if session_id is None or not isinstance(session_id, str):
             return None
 
-        user_id = self.user_id_for_session_id(session_id)
-
-        if user_id is None:
-            return None
-
-        user = User.get(user_id)
-
-        return user
+        return self.user_id_by_session_id.get(session_id)
