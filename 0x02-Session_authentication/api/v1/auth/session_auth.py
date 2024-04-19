609aeec4b61291526777595a7c5f0a914c4562d5
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-19 22:17:44 +0000

    upd

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,25 +3,45 @@
 Module for Session Authentication.
 module Session Authentication
 """
+import os
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-import uuid
+from api.v1.auth.basic_auth import BasicAuth
+from models.user import User
+from api.v1.auth.session_auth import SessionAuth
 
-class SessionAuth(Auth):
-    """Session Authentication"""
-    pass
-    user_id_by_session_id = {}
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-    def create_session(self, user_id: str = None) -> str:
-        """Create a Session ID for a user_id."""
-        if not isinstance(user_id, str):
-            return None
+@app.before_request
+def before_request():
+def before_request() -> None:
+    """Filtering each request."""
+    auth = None
+    auth_type = os.getenv('AUTH_TYPE')
 
-        session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Return a User ID based on a Session ID."""
-        if session_id is None or not isinstance(session_id, str):
-            return None
+    if auth_type == 'auth':
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    elif auth_type == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    elif auth_type == 'session_auth':
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
 
-        return self.user_id_by_session_id.get(session_id)
+    if auth:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if request.path not in excluded_paths and auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+    # Assign the result of auth.current_user(request) to request.current_user
+    request.current_user = auth.current_user(request) if auth else None
+
+app.before_request(before_request)
+
+if __name__ == "__main__":
+    host = os.getenv('API_HOST', '0.0.0.0')
