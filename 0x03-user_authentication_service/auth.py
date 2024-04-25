158ed3db6794bdaf8dc7f5fd27ebd233752f3836
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-25 17:36:29 +0000

    task9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """module for related-authentication routine
 """
+
 import bcrypt
 from db import DB
-from user import User
-
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -26,33 +25,15 @@ class Auth:
         hashed_password = bcrypt.hashpw(password.encode(), salt)
         return hashed_password
 
-    def register_user(self, email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> None:
         """Registers a new user.
 
         Args:
             email: A string representing the user's email.
             password: A string representing the user's password.
-
-        Returns:
-            User: A User object representing the registered user.
-
-        Raises:
-            ValueError: If a user already exists with the passed email.
         """
-        # Check if user already exists
-        if self._db.find_user_by_email(email):
-            raise ValueError(f"User {email} already exists")
-
-        # Hash the password
         hashed_password = self._hash_password(password)
-
-        # Create a new User object
-        user = User(email, hashed_password)
-
-        # Save the user to the database
-        self._db.add_user(user)
-
-        return user
+        self._db.add_user(email, hashed_password)
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates user login credentials.
