a3307eb2feeae6ac3e01dc6e6ec0b392c54f137f
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-25 17:47:55 +0000

    task11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app with user authentication feature
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -17,6 +17,18 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
+
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response, 200
+    else:
+        abort(401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
